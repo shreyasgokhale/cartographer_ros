eb4ee037a15605af637c9021e24f6e872798cad0
cschuet <cschuet@google.com>
2018-04-11 08:10:52 +0200

    WIP

--- cartographer_ros/configuration_files/backpack_2d_server_cloud.lua
+++ cartographer_ros/configuration_files/backpack_2d_server_cloud.lua
@@ -15,6 +15,6 @@
 include "map_builder_server.lua"
 
 MAP_BUILDER_SERVER.map_builder.use_trajectory_builder_2d = true
-MAP_BUILDER_SERVER.uplink_server_address = "127.0.0.1:50052"
+MAP_BUILDER_SERVER.server_address = "0.0.0.0:50052"
 
 return MAP_BUILDER_SERVER
